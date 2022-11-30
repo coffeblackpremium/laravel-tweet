0c3bc3229897a7a68a5631dfab14b6747ab0f7e1
João <joaodamaceno188@gmail.com>
2022-11-30 16:39:39 -0400

    feat: :sparkles: Cria like e relaciona com Tweet e User
    Um novo model é criado para like

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -5,19 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tweet extends Model
+class Like extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['content'];
+    protected $fillable = ['user_id', 'tweet_id'];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    public function likes()
+    public function tweet()
     {
-        return $this->hasMany(Like::class);
+        return $this->belongsTo(Tweet::class);
     }
 }
