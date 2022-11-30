825bba87ff1faaa318efea84f5dbef281ec19f43
João <joaodamaceno188@gmail.com>
2022-11-30 15:35:53 -0400

    feat: :sparkles: Adiciona paginação dinamica

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,9 +1,22 @@
 <div>
     Show Tweets
-    <p> {{$message}} </p>
-    <input type="text" name="message" id="message" wire:model='message'/>
+    <p> {{$content}} </p>
+
+    <form method="post" wire:submit.prevent='create'>
+
+        <input type="text" name="content" id="content" wire:model='content'/>
+        <button type="submit">Criar Tweet</button>
+        @error('content')
+            {{$message}}
+        @enderror
+    </form>
 
     @foreach ($tweets as $tweet)
-        {{ $tweet->user->name}} - {{ $tweet->content }}
+        <p>{{ $tweet->user->name}} - {{ $tweet->content }}</p>
     @endforeach
+
+    <hr>
+    <div> 
+        {{ $tweets->links() }}
+    </div>
 </div>
